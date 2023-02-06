e4d80161e27814678278f598cc7282b70c0d2091
an-gj-dy <ante.dybedahl@gmail.com>
2023-02-07 00:28:09 +0100

    Minor updates

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+// Listens for click on Hamburger icon (For mobile)
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+});
+
 // Observe elements to animate once in view
 const pages = Array.from(
     document.querySelectorAll("section:not(:first-child)")
 );
 
-const observer = new IntersectionObserver(
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("in-view");
-            }
-        });
-    },
-    { threshold: 0.4 }
-);
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("in-view");
+        }
+    });
+});
 
 pages.forEach((page) => {
     observer.observe(page);
