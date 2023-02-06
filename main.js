521c67ce3c5ef1a0b7b04cc4b6747785608518d1
an-gj-dy <ante.dybedahl@gmail.com>
2023-02-06 10:54:43 +0100

    Checkpoint

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const pageArray = Array.from(document.querySelectorAll(".page"));
-const navigationArray = Array.from(
-    document.querySelectorAll(".sticky-navbar ul li")
-);
+const navigationArray = Array.from(document.querySelectorAll(".sticky-navbar ul li"));
+const navigationItemClassArray = [];
+
+navigationArray.forEach((item) => {
+    navigationItemClassArray.push(item.className);
+});
 
 navigationArray.forEach((listItem, index) => {
     listItem.addEventListener("click", () => {
@@ -20,8 +23,12 @@ function addObserver(selector, options = {}) {
 function observerEvent(element, options) {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
+            const observedElementClassArray = Array.from(entry.target.classList);
             if (entry.isIntersecting) {
-                console.log(entry.target.classList);
+                for (let i = 0; i < observedElementClassArray.length; i++) {
+                    for (let y = 0; y < navigationArray.length; y++) console.log(navigationArray[y].className.includes(observedElementClassArray[i]));
+                }
+                //navigationArray.filter((element) => observedElementClassArray.findIndex(element))
             }
         });
     }, options);
